dd9ce9a63421df72023a177c83e8aabc86b7ce90
IfeMartin <114827202+IfeMartin@users.noreply.github.com>
2025-01-30 17:47:49 -0500

    Second commit.

--- LSP_Assignment1/src/org/howard/edu/lsp/assugnment1/HelloWorld.java
+++ LSP_Assignment1/src/org/howard/edu/lsp/assugnment1/HelloWorld.java
@@ -2,8 +2,12 @@ package org.howard.edu.lsp.assugnment1;
 
 public class HelloWorld {
 
+	/**
+	 * Ife Martin 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World! Hello Prof. Woolfolk!");
 
 	}
 
