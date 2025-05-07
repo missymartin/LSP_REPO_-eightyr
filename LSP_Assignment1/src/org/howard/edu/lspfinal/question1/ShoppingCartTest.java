f2adef19460f3e9e346ea6c97c06dfe7b05acc5e
missymartin <ife.martin@bison.howard.edu>
2025-05-06 22:53:55 -0400

    Manual Question 1 code push

--- LSP_Assignment1/src/org/howard/edu/lspfinal/question1/ShoppingCartTest.java
+++ LSP_Assignment1/src/org/howard/edu/lspfinal/question1/ShoppingCartTest.java
@@ -17,38 +17,29 @@ class ShoppingCartTest {
 	
 	@BeforeEach
 	void setUp(){
+		cart1 = new ShoppingCart();
 		cart1.addItem("Bannana", 2.0);
 		cart1.addItem("Apple", 1.0);
 		
+		cart2 = new ShoppingCart();
+		
+	}
+	
+	@Test
+	@DisplayName("Test adding Items.")
+	public void testAddItem() {
+		//Valid item, since the add Item is private we are using the total cost method to check the new item was added
+		//orginal cost of cart 3.0
+		cart1.addItem("Avacado", 4.0);
+		assertEquals(cart1.getTotalCost(),7.0);
+		//negative item
+		Throwable exception = assertThrows( IllegalArgumentException.class, ()->cart1.addItem("Air", -34.0));
+		assertEquals("Price cannot be negative.", exception.getMessage());
+		//0 price item
+		Throwable exception2 = assertThrows( IllegalArgumentException.class, ()->cart1.addItem("Nothing", 0.0));
+		assertEquals("Price cannot be negative.", exception2.getMessage());
 	}
 
-	@Test
-	@DisplayName("Test total cost.")
-	public void testTotalCost() {
-		//full cart
-		assertEquals(cart1.getTotalCost(),3.0);
-		//empty cart
-		assertEquals(cart2.getTotalCost(),3.0);
-		//discount cart
-		cart1.applyDiscountCode("SAVE10");
-		assertEquals(cart1.getTotalCost(),3.0);
-	}
-	
-	@Test
-	@DisplayName("Test remove items.")
-	public void testRemoveItem() {
-		//full cart
-		cart1.removeItem("Apple");
-		assertEquals(cart1.getTotalCost(),2.0);
-		//empty cart
-		Throwable exception = assertThrows( IllegalArgumentException.class, ()->cart2.removeItem("Apple"));
-		assertEquals("Item does not exist.", exception.getMessage());
-		//not in cart
-		
-		Throwable exception2 = assertThrows( IllegalArgumentException.class, ()->cart1.removeItem("Avacado"));
-		assertEquals("Item does not exist.", exception2.getMessage());;
-	}
-	
 	@Test
 	@DisplayName("Test applying discount codes.")
 	public void testDiscountCode() {
@@ -65,15 +56,30 @@ class ShoppingCartTest {
 	}
 	
 	@Test
-	@DisplayName("Test adding Items.")
-	public void testAddItem() {
-		//Valid item, since the add Item is private we are using the total cost method to check the new item was added
-		//orginal cost of cart 3.0
-		cart1.addItem("Avacado", 4.0);
-		assertEquals(cart1.getTotalCost(),7.0);
-		//negative item
-		Throwable exception = assertThrows( IllegalArgumentException.class, ()->cart1.addItem("Air", -34.0));
-		assertEquals("Price cannot be negative.", exception.getMessage());
+	@DisplayName("Test total cost.")
+	public void testTotalCost() {
+		//full cart
+		assertEquals(cart1.getTotalCost(),3.0);
+		//empty cart
+		assertEquals(cart2.getTotalCost(),0.0);
+		//discount cart
+		cart1.applyDiscountCode("SAVE10");
+		assertEquals(cart1.getTotalCost(),2.7);
 	}
+	
+	@Test
+	@DisplayName("Test remove items.")
+	public void testRemoveItem() {
+		//full cart
+		cart1.removeItem("Apple");
+		assertEquals(cart1.getTotalCost(),2.0);
+		//empty cart
+		Throwable exception = assertThrows( IllegalArgumentException.class, ()->cart2.removeItem("Apple"));
+		assertEquals("Item does not exist.", exception.getMessage());
+		//not in cart
+		Throwable exception2 = assertThrows( IllegalArgumentException.class, ()->cart1.removeItem("Avacado"));
+		assertEquals("Item does not exist.", exception2.getMessage());;
+	}
+	
 
 }
